bfaf38812f6abb8ab984a7f2d6cac98c9b6e32fb
Dyego dos Santos Almeida <dyegocm@gmail.com>
2021-02-08 09:23:20 -0300

    Atualização index.
    Alterar parâmetros para padrões.

--- test/index.php
+++ test/index.php
@@ -5,13 +5,9 @@ require __DIR__.'/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email(2,'smtp-web.kinghost.net','dyego@cmhfarmaceutica.com.br','sucesso*0800','tls','587','dyego@cmhfarmaceutica.com.br','Dyego CMH');
-$novoEmail->sendMail("Assunto de Teste",
-                     "<p>Esse é um email de <b>teste</b>!</p>",
-                     "dyego@cmhfarmaceutica.com.br",
-                     "Dyego CMH",
-                     "dyegocm@gmail.com",
-                     "Dyego Gmail");
+$novoEmail = new Email(2, "mail.host.com", "your@email.com", "your-pass", "smtp secure (tls / ssl)", "port (587)",
+    "from@email.com", "From name");
+$novoEmail->sendMail("SUbject", "Content", "reply@email.com", "Replay Name", "address@email.com", "Address Name");
 
 var_dump($novoEmail);                     
 ?>
